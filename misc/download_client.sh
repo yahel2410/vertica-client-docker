c86baeefa67d3aaab6363923e8c0fb81ecb40b7b
yahel <yahel@twingo.co.il>
2018-08-27 18:18:10 +0300

    support 8.0, 9.0

--- misc/download_client.sh
+++ misc/download_client.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-if [ "$1" = '8.1' ]; then
+if [ "$1" = '8.0' ]; then
+    client='https://my.vertica.com/client_drivers/8.0.x/8.0.1/vertica-client-8.0.1-0.x86_64.tar.gz'
+elif [ "$1" = '8.1' ]; then
     client='https://my.vertica.com/client_drivers/8.1.x/8.1.1-13/vertica-client-8.1.1-13.x86_64.tar.gz'
+elif [ "$1" = '9.0' ]; then
+    client='https://my.vertica.com/client_drivers/9.0.x/9.0.1-4/vertica-client-9.0.1-4.x86_64.tar.gz'
 elif [ "$1" = '9.1' ] ; then
     client='https://my.vertica.com/client_drivers/9.1.x/9.1.1-0/vertica-client-9.1.1-0.x86_64.tar.gz'
 else
@@ -11,6 +15,6 @@ else
     exit 1
 fi
 
-wget $client
+wget $client > /dev/null
 tar -xvf vertica-client*.tar.gz -C /
 rm -f vertica-client*.tar.gz
